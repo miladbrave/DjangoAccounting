d93a760528b168b21222ffea19fc6cdf9335e657
miladbrave <milad.pourshoja@gmail.com>
2020-10-24 12:57:39 +0330

    requirements freeze

--- web/models.py
+++ web/models.py
@@ -6,4 +6,14 @@ class Expense(models.Model):
     text = models.CharField(max_length=255)
     date = models.DateTimeField()
     amount = models.BigIntegerField()
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return "{}------>{}".format(self.text, self.amount)
+
+class Income(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return "{}------>{}".format(self.text, self.amount)
